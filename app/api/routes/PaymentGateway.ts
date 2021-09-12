3dce9b2d1620a39e8aafc11e244bd0a61b3e0504
abrar <alisher5591241@gmail.com>
2021-09-12 14:57:42 -0500

    payment api ready

--- app/api/routes/PaymentGateway.ts
+++ app/api/routes/PaymentGateway.ts
@@ -18,7 +18,7 @@ export default (app: Router) => {
       try {
         
          const response = await PaymentFunction.getMerchantDetails(req.body,function (data){
-           res.status(200).json({ "responce": data });
+           res.status(200).json({ ...data });
          });
           
       } catch (e) {
